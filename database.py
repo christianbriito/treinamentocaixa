f95d4a4af02590f9e91b861189921959d1e9073c
Christian Brito <chrstnbrito34@gmail.com>
2020-12-17 02:01:18 -0300

    Alteraçao do banco de dados

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 import sqlite3
 
-conn = sqlite3.connect('clientes.db')
+conn = sqlite3.connect('usuarios.db')
 
 cursor = conn.cursor()
 
 cursor.execute("""
-CREATE TABLE clientes (
+CREATE TABLE usuarios (
         id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
         nome TEXT NOT NULL,
         password TEXT NOT NULL,
